79891a69cc98c7c66eddd79bc4f4f749a8ebf083
unknown <golu17012001@gmail.com>
2026-04-06 14:50:25 +0530

    frontend changed

--- server/index.js
+++ server/index.js
@@ -1923,7 +1923,7 @@ const app = express();
 // }));
 const allowedOrigins = [
   "http://localhost:5173",
- 
+    "https://gray-pebble-06934421e.6.azurestaticapps.net",
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
@@ -2381,7 +2381,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-     
+       "https://gray-pebble-06934421e.6.azurestaticapps.net",
       process.env.FRONTEND_URL,
     ].filter(Boolean),
     methods: ["GET", "POST"],
